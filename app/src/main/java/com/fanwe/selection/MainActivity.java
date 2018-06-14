9a770dacbb5fee6846ae469e9e3f708fe84c9492
zjun <565061763@qq.com>
2018-06-14 17:11:01 +0800

    demo

--- app/src/main/java/com/fanwe/selection/MainActivity.java
+++ app/src/main/java/com/fanwe/selection/MainActivity.java
@@ -1,14 +1,50 @@
 package com.fanwe.selection;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.TextView;
+
+import com.fanwe.lib.selection.config.FViewSelectConfig;
+import com.fanwe.lib.selection.config.SelectConfig;
+import com.fanwe.lib.selection.properties.TextViewProperties;
 
 public class MainActivity extends AppCompatActivity
 {
+    private TextView mTextView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mTextView = findViewById(R.id.textview);
+        mTextView.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                // 切换选中状态
+                mTextView.setSelected(!mTextView.isSelected());
+            }
+        });
+
+        FViewSelectConfig.config(mTextView).init(new SelectConfig.PropertiesIniter<TextViewProperties>()
+        {
+            @Override
+            public void init(TextViewProperties normal, TextViewProperties selected)
+            {
+                // 配置正常状态的参数
+                normal.setTextColor(Color.BLACK)
+                        .setTextSize(40)
+                        .setAlpha(0.5f);
+
+                // 配置选中状态的参数
+                selected.setTextColor(Color.RED)
+                        .setTextSize(60)
+                        .setAlpha(1.0f);
+            }
+        }).setSelected(false);
     }
 }
